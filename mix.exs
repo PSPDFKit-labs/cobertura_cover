adcf0214c43c4c17dda5e1b910e25059c49e6a7d
Martin Schürrer <martin@schuerrer.org>
2015-03-17 17:06:29 +0000

    Add hex package spec.

--- mix.exs
+++ mix.exs
@@ -2,18 +2,32 @@ defmodule CoberturaCover.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cobertura_cover,
-     version: "0.9.0",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+      app: :cobertura_cover,
+      version: "0.9.0",
+      elixir: "~> 1.0",
+      deps: [],
+      source_url: "https://github.com/PSPDFKit-labs/cobertura_cover",
+      description: description,
+      package: package,
+    ]
   end
 
   def application do
     [applications: [:logger]]
   end
 
-  defp deps do
-    [
-    ]
+  defp description do
+    """
+    A plugin for `mix test --cover` that writes a `coverage.xml` file compatible with Jenkins'
+    Cobertura plugin.
+    """
+  end
+
+  defp package do
+    [contributors: ["Martin Schurrer"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/PSPDFKit-labs/cobertura_cover"},
+     files: ["lib", "mix.exs", "README.md"]]
   end
 end
